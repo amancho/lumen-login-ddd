a98912e2ae092f930681eec066e047487a0f73ee
amancho <amancho@intercomempresas.com>
2019-12-10 08:35:43 +0100

    Add user class

---
+++ Domain/Entities/User.php
@@ -0,0 +1,37 @@
+<?php
+namespace Domain\Entities;
+
+/**
+ * User
+ */
+class User
+{
+    /**
+     * @var integer
+     */
+    private $id;
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $email;
+
+    /**
+     * @var string
+     */
+    private $password;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt = 'CURRENT_TIMESTAMP';
+
+    /**
+     * @var \DateTime
+     */
+    private $deletedAt;
+}
